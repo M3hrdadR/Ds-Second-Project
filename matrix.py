16ebf15ac91bc838dac1c822451660ee8b0d9608
Mehrdad <mehrdadriyahi1378@gmail.com>
2019-02-12 22:45:12 +0330

    Project Done!

--- matrix.py
+++ matrix.py
@@ -34,7 +34,6 @@ def max_matrix(a, n, m):
                             max_index_j = co
                             horizontal_edge = row
                             vertical_edge = col
-    print(max_area)
     for i in range(horizontal_edge + 1):
         for j in range(vertical_edge + 1):
             print(a[i + max_index_i][j + max_index_j], end=" ")
