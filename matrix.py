0190affdaa15113b93743282a0c29e49b7864d37
Mehrdad <mehrdadriyahi1378@gmail.com>
2019-02-12 22:44:43 +0330

    Project Done!

--- matrix.py
+++ matrix.py
@@ -4,9 +4,8 @@ import random
 def read_matrix(a):
     for i in range(len(a)):
         for j in range(len(a[i])):
-            # a[i][j] = int(input("Enter the %d row and %d column:" % (i, j)))
-            a[i][j] = random.randint(0, 100) % 2
-            return a
+            a[i][j] = int(input("Enter the element of %d row and %d column:" % (i, j)))
+    return a
 
 
 def max_matrix(a, n, m):
@@ -21,27 +20,33 @@ def max_matrix(a, n, m):
                 for co in range(m - col):
                     error = 0
                     tmp = a[ro][co]
-                    for r in range(row):
-                        for c in range(col):
+                    for r in range(row + 1):
+                        for c in range(col + 1):
                             if a[r + ro][c + co] != tmp:
                                 error = 1
                                 break
                         if error:
                             break
-                        if row * col > max_area:
-                            max_area = row * col
+                    if not error:
+                        if (row + 1) * (col + 1) > max_area:
+                            max_area = (row + 1) * (col + 1)
                             max_index_i = ro
                             max_index_j = co
                             horizontal_edge = row
                             vertical_edge = col
-    for i in range(horizontal_edge):
-        for j in range(vertical_edge):
+    print(max_area)
+    for i in range(horizontal_edge + 1):
+        for j in range(vertical_edge + 1):
             print(a[i + max_index_i][j + max_index_j], end=" ")
         print()
 
 
 n = int(input("Enter the number of rows matrix:"))
 m = int(input("Enter the number of columns matrix:"))
-a = [[0 for j in range(n)]for i in range(n)]
+a = [[0 for j in range(m)]for i in range(n)]
 a = read_matrix(a)
-max_matrix(a)
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        print(a[i][j], end=" ")
+    print()
+max_matrix(a, n, m)
